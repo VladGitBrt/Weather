8c65c6c558a6e97035c9975a3bc53effc6ec27ba
VladBrt <vladikjbortnik@gmail.com>
2021-03-25 11:39:23 +0200

    Weather repos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import {GetWeatherService} from './get-weather.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css' ]
 })
 export class AppComponent{
   countries = [];
